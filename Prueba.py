ddf0abaa7ec03c7a55c94349316880923c7eeec4
Kendall <kendallmadrigal14@gmail.com>
2023-02-21 12:28:33 -0600

    Pantalla inicio de sesión

--- Prueba.py
+++ Prueba.py
@@ -23,4 +23,31 @@ def VentanaCorreoUnico():
 
     ventana.mainloop()
 
-VentanaCorreoUnico()
+
+def IniciarSesion():
+    ventana = tk.Tk()
+    ventana.title("Inicio de sesión")
+    ventana.geometry("500x250")
+
+    titulo_label = tk.Label(ventana, text="Inicio de sesión", font=("Arial", 18), pady=20)
+    titulo_label.pack()
+
+    correo_label = tk.Label(ventana, text="Correo electrónico:", anchor="center")
+    correo_label.pack()
+    correo_entry = tk.Entry(ventana, width=50, justify="center")
+    correo_entry.pack()
+
+    contrasena_label = tk.Label(ventana, text="Contraseña generada:", anchor="center")
+    contrasena_label.pack()
+    contrasena_entry = tk.Entry(ventana, width=50, show="*", justify="center")
+    contrasena_entry.pack()
+
+    iniciar_sesion_button = tk.Button(ventana, text="Iniciar sesión")
+    iniciar_sesion_button.pack(pady=20)
+
+    # centrar la ventana en la pantalla
+    ventana.eval('tk::PlaceWindow %s center' % ventana.winfo_toplevel())
+
+    ventana.mainloop()
+
+IniciarSesion()
